5b9df073a38d68b38cb63020ddc817b58e26b59c
ivan-bogach <catmrgerman@gmail.com>
2018-09-10 19:30:24 +0300

    first commit

---
+++ brain-ring-lib/source/models/client.h
@@ -0,0 +1,13 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include "brain-ring-lib_global.h"
+
+class BRAINRINGLIBSHARED_EXPORT Client
+{
+
+public:
+    Client();
+};
+
+#endif // CLIENT_H
---
+++ brain-ring.pro
@@ -0,0 +1,6 @@
+TEMPLATE = subdirs
+
+SUBDIRS += \
+    brain-ring-ui \
+    brain-ring-lib \
+    brain-ring-tests
